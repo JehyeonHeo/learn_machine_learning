d9b4181951ce51ed7b91b1283575af831ffff534
JehyeonHeo <jhheo123123@gmail.com>
2017-09-18 21:16:36 +0900

    complete outlierCleaner function

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -12,9 +12,25 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     
     cleaned_data = []
-
+    
     ### your code goes here
+    errors = []
+    for i in range(len(predictions)):
+        error = predictions[i] - net_worths[i]
+        errors.append(error)
+        
+    abs_errors = [abs(error) for error in errors]
+    tuples = []
+    for i in range(len(abs_errors)):
+        tuples.append((ages[i][0], net_worths[i][0], abs_errors[i][0]))
 
+    tuples = sorted(tuples, key = lambda tuples : tuples[2])
+
+    for j in range(int(len(tuples) * 0.9)):
+        cleaned_data.append(tuples[j])
+
+    for data in cleaned_data:
+        print data
     
     return cleaned_data
 
