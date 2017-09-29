cadbc936ed48e446ba8219a3f36e5363cdbf3e98
JehyeonHeo <jhheo123123@gmail.com>
2017-09-29 10:03:50 +0900

    make poi_id.py have same contents as ipynb file

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -10,14 +10,24 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi','bonus', 'exercised_stock_options'] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+data_dict.pop('TOTAL', 0)
+
 ### Task 3: Create new feature(s)
+for name, data in data_dict.iteritems():
+    restricted_stock = data['restricted_stock']
+    exercised_stock_options = data['exercised_stock_options']
+    if restricted_stock != 'NaN' and exercised_stock_options != 'NaN':
+        data['total_stock_value_without_deferred'] = restricted_stock + exercised_stock_options
+    else:
+        data['total_stock_value_without_deferred'] = 'NaN'
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
@@ -32,8 +42,8 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+from sklearn.tree import DecisionTreeClassifier
+dtClf = DecisionTreeClassifier()
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -43,9 +53,13 @@ clf = GaussianNB()
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+feature_train, feature_test, label_train, label_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
+
+dt_parameters = {'min_samples_split' : [2, 5, 10, 15], 'criterion' : ['gini', 'entropy']}
+clf = GridSearchCV(dtClf, dt_parameters)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
